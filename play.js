34f2654dabe2cc3fa25fd6ab6f8b48b567edf91d
Ebele <ebele.omanukwue@telus.com>
2022-11-23 10:26:39 -0700

    my first game connection

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require("net"); 
+
+
+// establishes a connection with the game server
+const connect = function (data) {
+  console.log("information:")
+const conn = net.createConnection({
+  host: "172.27.168.139",
+  port: 50541,
+});
+
+conn.setEncoding("utf8"); // interpret data as text
+
+return conn;
+};
+
+console.log("Connecting ...");
+connect();
