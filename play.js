3faff3a2720bd6ccbf7270981269a07869c959bc
EbeleOmanukwue <116769302+eby84@users.noreply.github.com>
2022-11-23 13:50:40 -0700

    Update play.js

--- play.js
+++ play.js
@@ -2,14 +2,14 @@ const net = require("net");
 
 
 // establishes a connection with the game server
-const connect = function (data) {
-  console.log("information:")
+const connect = function () {
+  
 const conn = net.createConnection({
   host: "172.27.168.139",
   port: 50541,
 });
 conn.on('data', (message) => {
-  console.log(message);
+  console.log("New message:", message);
 conn.setEncoding("utf8"); // interpret data as text
 
 return conn;
