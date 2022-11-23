e71301cc94b573a170c524454a4e2c3333ddd0db
Ebele <ebele.omanukwue@telus.com>
2022-11-23 13:50:03 -0700

    update

--- client.js
+++ client.js
@@ -1,19 +1,17 @@
  
 
 const net = require("net");
-
-const host = "172.27.168.139";
-const port = 50541;
-const connectionConfig = {
-  host,
-  port
-};
 // establishes a connection with the game server
-const connect = () => {
-const conn = net.createConnection(connectionConfig);
-conn.on('data', (message) => {
-  console.log(message);
+const connect = function () {
+  const conn = net.createConnection({
+  host: "172.27.168.139",
+  port: 50541,
+});
+
+ conn.on('data', (message) => {
+  console.log("New message:", message);
   });
+  
 conn.setEncoding("utf8"); // interpret data as text
 
 return conn;
@@ -22,10 +20,8 @@ return conn;
 console.log("Connecting ...");
 connect();
 
-module.exports = {
-  host,
-  port 
-};
+
+module.export = {connect};
 
 
 
